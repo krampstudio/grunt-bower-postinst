27a35d882e04d8eb267a751a79c9d9dafdbb1edd
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2013-06-18 07:56:59 -0400

    set up action detection

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,32 +20,19 @@ module.exports = function(grunt) {
       ],
       options: {
         jshintrc: '.jshintrc',
-      },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
+      }
     },
 
     // Configuration to be run (and then tested).
     bower_postinst: {
-      default_options: {
+      dist: {
         options: {
-        },
-        files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!',
-        },
-        files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123'],
-        },
-      },
+            components : [
+                'jquery.ui',
+                'bootstrap'
+            ]
+        }
+      }
     },
 
     // Unit tests.
@@ -55,17 +42,16 @@ module.exports = function(grunt) {
 
   });
 
-  // Actually load this plugin's task(s).
+  //Load the plugin's task.
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'bower_postinst', 'nodeunit']);
+  grunt.registerTask('test', ['nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
--- tasks/bower_postinst.js
+++ tasks/bower_postinst.js
@@ -10,48 +10,34 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
-
-  grunt.registerMultiTask('bower_postinst', 'Post Installation Tasks for Bower Components.', function() {
+    grunt.registerMultiTask('bower_postinst', 'Post Installation Tasks for Bower Components.', function() {
       
-    var bower = require('bower');
-
-    var options = this.options({
-        directory : bower.config.directory,
-        components : []
-    });
+        var bower = require('bower'),
+            util = require('util');
     
-    
-    
-    /*
-
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
+        var options = this.options({
+            directory : bower.config.directory,
+            components : []
+        });
         
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
-        }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
-
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
-    });*/
-  });
-
+        options.components.forEach(function(component){
+            var compDir =  options.directory + "/" + component;
+            if(grunt.file.exists(compDir) && grunt.file.isDir(compDir)){
+    
+                //discover available actions
+                var build = {
+                    gitsm   : grunt.file.exists(compDir + "/.gitmodules"),
+                    npm     : grunt.file.exists(compDir + "/package.json"),
+                    grunt   : grunt.file.exists(compDir + "/Gruntfile.js"),
+                    jake    : grunt.file.exists(compDir + "/Jakefile"),
+                    make    : grunt.file.exists(compDir + "/Makefile")
+                };
+                
+                grunt.log.debug("Entering " + compDir  + " -> " + util.inspect(build));
+                
+            } else {
+                grunt.log.warn('Component "' + component + '" not found in ' + compDir );
+            }
+        });
+    });
 };
