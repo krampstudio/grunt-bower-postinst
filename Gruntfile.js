b2880659ab0e488b62481bd44566adcf1d0928e6
Bertrand CHEVRIER <chevrier.bertrand@gmail.com>
2013-07-04 08:04:24 -0400

    update build to automate bower install

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,50 +10,49 @@
 
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        'tasks/*.js',
-        '<%= nodeunit.tests %>',
-      ],
-      options: {
-        jshintrc: '.jshintrc',
-      }
-    },
-
-    // Configuration to be run (and then tested).
-    bower_postinst: {
-      dist: {
-        options: {
-            components : {
-                'jquery.ui' : ['npm', {'grunt' : 'build'}],
-                'bootstrap' : ['npm', {'make': []} ]
+    grunt.initConfig({
+        jshint: {
+            all: ['Gruntfile.js', 'tasks/*.js', '<%= nodeunit.tests %>', ],
+            options: {
+                jshintrc: '.jshintrc',
             }
+        },
+        
+        clean : ['components'],
+        
+        bower : {
+            install : {}  
+        },
+        
+        bower_postinst: {
+            dist: {
+                options: {
+                    components: {
+                        'jquery.ui': ['npm', {'grunt': 'build'}],
+                        'bootstrap': ['npm', {'make': [] }]
+                    }
+                }
+            }
+        },
+        
+        // Unit tests.
+        nodeunit: {
+            tests: ['test/*_test.js'],
         }
-      }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
-  });
-
-  //Load the plugin's task.
-  grunt.loadTasks('tasks');
-
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['nodeunit']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+    
+    });
+    
+    //Load the plugin's task.
+    grunt.loadTasks('tasks');
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-nodeunit');
+    grunt.loadNpmTasks('grunt-bower-task');
+    
+    grunt.registerTask('bower-full', ['clean', 'bower', 'bower_postinst']);
+    grunt.registerTask('test', ['bower-full', 'nodeunit']);
+    grunt.registerTask('default', ['jshint', 'test']);
 
 };
+  
